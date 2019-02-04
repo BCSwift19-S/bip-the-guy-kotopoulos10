b61f89d2fb31e1be598f7b6787c5ff0ce0277e96
Tom Kotopoulos <kotopoulos10@gmail.com>
2019-02-04 16:16:29 -0500

    4.2 Complete

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageToPunch: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func libraryPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func takePhotoPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func imagePressed(_ sender: UITapGestureRecognizer) {
+        animateImage()
+    }
+    
+    func animateImage(){
+        
+        let bounds = self.imageToPunch.bounds
+        let shrinkValue: CGFloat = 60
+        
+        //shrink our imageToPunch by 60pxs
+        self.imageToPunch.bounds = CGRect(x: self.imageToPunch.bounds.origin.x + shrinkValue , y: self.imageToPunch.bounds.origin.y + shrinkValue , width: self.imageToPunch.bounds.size.width - shrinkValue, height: self.imageToPunch.bounds.size.height - shrinkValue)
+        
+        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: { self.imageToPunch.bounds = bounds }, completion: nil)
+    }
+    
+    
 }
 
